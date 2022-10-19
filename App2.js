2972f7573eba6ce5af6ac8d343682d18975bec8d
carnaubarosario <31799642+carnaubarosario@users.noreply.github.com>
2022-10-18 23:23:38 -0300

    Add files via upload
    Template para tela GET

---
+++ App2.js
@@ -0,0 +1,67 @@
+import './App.css';
+import React, { useState } from 'react';
+import Axios from "axios";
+
+function App() {
+    const [values, setValues] = useState();
+
+    const handleChangeValues = (value) => {
+        setValues((prevValue) =>({
+            ...prevValue,
+            [value.target.name]: value.target.value,
+
+        }));
+        console.log(values)
+    };
+
+    const handleClickButton = () =>{
+        Axios.post("localhost:3000/cadastrar-livro", {
+            titulo: values.titulo,
+            autor:  values.autor,
+            isbn:   values.isbn,
+            resumo: values.resumo,
+            ano_lancamento: values.ano_lancamento,
+        }).then((response) => {
+            console.log(response);
+        });
+    };
+
+    return (
+        <main className="container">
+            <h2>Título do Livro</h2>
+            <form action="">
+                <div className="input-field">
+                    <p>Esse é o título do livro: </p>
+                    <div className="underline"></div>
+                </div>
+
+                <div className="input-field">
+                    <p>Esse é o autor do livro: </p>
+                    <div className="underline"></div>
+                </div>
+
+                <div className="input-field">
+                    <p>Esse é o ISBN do livro: </p>
+                    <div className="underline"></div>
+                </div> 
+
+                <div className="input-field">
+                    <p>Esse é o resumo do livro:</p>
+                    <div className="underline"></div>
+                </div>
+
+                <div className="input-field">
+                    <p>Esse é o ano de Lançamento do livro: </p>
+                    <div className="underline"></div>
+                </div>
+
+            </form>
+
+    </main>
+     
+ 
+    );
+  }
+
+  
+  export default App2;
